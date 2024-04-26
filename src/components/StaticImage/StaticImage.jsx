1c04f25d6519c411e1b38d6e2a2d9b33dea6d3bf
Celinezzzzz <99721409+Celinezzzzz@users.noreply.github.com>
2024-04-25 23:18:03 -0700

    Revert "Feature static image page"

--- src/components/StaticImage/StaticImage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './StaticImage.scss';
-
-const StaticImage = ({ image }) => {
-  return <img className="static-image" src={image} alt="Description" />;
-};
-
-export default StaticImage;
