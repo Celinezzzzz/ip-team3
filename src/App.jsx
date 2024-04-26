f616b1acbef00404a21e136b5d5a76e346b9c5d5
Celine Zhou <celine_zhou@outlook.com>
2024-04-25 23:16:04 -0700

    added static image

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.scss";
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header/Header";
+import ListedItemsPage from './pages/ListedItemsPage/ListedItemsPage';
+import SoldItemsPage from './pages/SoldItemsPage/SoldItemsPage';
 import StatusPage from './pages/StatusPage/StatusPage';
 //import Footer from "./components/Footer/Footer"; // Uncomment if Footer is used
 
@@ -11,6 +13,8 @@ function App() {
       <Header />
       <Routes>
         <Route path='/user-profile/status' element={<StatusPage/>} ></Route>
+        <Route path='/user-profile/listeditems' element={<ListedItemsPage/>} ></Route>
+        <Route path='/user-profile/solditems' element={<SoldItemsPage/>} ></Route>
       </Routes>
     </BrowserRouter>
   );
